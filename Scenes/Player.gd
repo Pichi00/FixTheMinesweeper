f7b0ffe448016640211762d5c59ba3a6f4e94252
Piotr Kołodziejski <piter.kolodziejski@gmail.com>
2021-03-18 15:44:29 +0100

    Change character's appearance

--- Scenes/Player.gd
+++ Scenes/Player.gd
@@ -13,3 +13,22 @@ func _physics_process(delta):
 	
 	input_vector = input_vector.normalized()
 	input_vector = move_and_slide(input_vector * speed)
+
+func _input(event):
+	if Input.is_action_pressed("ui_up"):
+		$AnimationPlayer.play("Upside")
+		$AnimatedSprite.animation = "GoUp"
+	elif Input.is_action_pressed("ui_down"):
+		$AnimationPlayer.play("Downside")
+		$AnimatedSprite.animation = "GoDown"
+	elif Input.is_action_pressed("ui_left"):
+		$AnimationPlayer.play("Left")
+		$AnimatedSprite.animation  = "GoSide"
+		$AnimatedSprite.flip_h = true
+	elif Input.is_action_pressed("ui_right"):
+		$AnimationPlayer.play("Right")
+		$AnimatedSprite.animation = "GoSide"
+		$AnimatedSprite.flip_h = false
+
+func attack():
+	pass
