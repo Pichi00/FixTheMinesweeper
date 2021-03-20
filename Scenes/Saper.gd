77f411ec75d1549e79af99c6c8eb9ba99c65cc95
Piotr Kołodziejski <piter.kolodziejski@gmail.com>
2021-03-20 10:51:49 +0100

    Add more puzzles

--- Scenes/Saper.gd
+++ Scenes/Saper.gd
@@ -1,6 +1,13 @@
 extends Node2D
 
 var logo_drag = false
+var square_drag = false
+var hour = 12
+var minute = 0
+var sec = 0
+var hh
+var mm
+var ss
 
 func _ready():
 	$Credits/JamLogo.input_pickable = true
@@ -14,6 +21,8 @@ func _input(event):
 func _physics_process(_delta):
 	if Input.is_mouse_button_pressed(BUTTON_LEFT) and logo_drag:
 		$Credits/JamLogo.position = get_local_mouse_position()
+	if Input.is_mouse_button_pressed(BUTTON_LEFT) and square_drag:
+		$Terminal/BlackSquare.position = get_local_mouse_position()
 
 func _on_Minesweeper_pressed():
 	$MineSweeperWindow.show()
@@ -50,3 +59,49 @@ func _on_JamLogo_mouse_exited():
 
 func _on_Credits_pressed():
 	$Credits.show()
+
+func _on_TwitterButton_pressed():
+	OS.shell_open("https://twitter.com/pichiDev")
+
+func _on_ItchButton_pressed():
+	OS.shell_open("https://pichidev.itch.io/")
+
+
+func _on_EnterButton_pressed():
+	if $Terminal/CommandInput.text == "light":
+		$Terminal/ColorRect.color = Color(255,255,255)
+	else:
+		$Terminal/CommandLabel.text = "\n Unknown command"
+
+func _on_TerminalButton_pressed():
+	$Terminal.show()
+
+func _on_BlackSquare_mouse_entered():
+	square_drag = true
+
+func _on_BlackSquare_mouse_exited():
+	square_drag = false
+
+
+func _on_Timer_timeout():
+	sec += 1
+	if sec == 60:
+		sec = 0
+		minute += 1
+		if minute == 60:
+			hour += 1
+			if hour == 24:
+				hour = 0
+	if hour<10:
+		hh = "0"+str(hour)
+	else:
+		hh = str(hour)
+	if minute<10:
+		mm = "0"+str(minute)
+	else:
+		mm = str(minute)
+	if sec<10:
+		ss = "0"+str(sec)
+	else:
+		ss = str(sec)
+	$SystemBar/Time.text = hh+" : "+mm+" : "+ss
